b62a150f6186d82f0dfcfe85842887e7fb529a04
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-04-25 22:54:23 +0700

    feature: add ssr support

--- src/server/types.d.ts
+++
@@ -1,9 +0,0 @@
-import type { User } from "next-auth";
-
-export type TLoginData = {
-  // TODO fix temporary data type
-  // data: any;
-  user: TUser & { name: string };
-  access_token: string;
-  expires_at: string;
-} & User;
